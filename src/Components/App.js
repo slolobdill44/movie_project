333e5dcba2467f2e809720344e50eb82594ffb2d
Adrian Lobdill <slolobdill44@gmail.com>
2017-10-01 12:54:11 -0700

    All information procedurally loads onto page

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,6 +5,7 @@ import MovieTile from './MovieTile';
 import { reviews, movies } from '../data.json';
 
 //css
+require('style-loader!./Reset.css');
 require('style-loader!./App.css');
 
 class App extends React.Component {
@@ -22,29 +23,35 @@ class App extends React.Component {
     this.setState({reviews: reviews});
     this.setState({movies: movies});
     // console.log(this.state);
-    // console.log(movies);
+    console.log(movies);
   }
 
   render() {
     const movieList = this.state.movies.map(movie => {
+      let userReview = this.state.reviews.find(movieReview => {
+        return movieReview["movie-id"] === movie.id
+      });
+
       return (
         <MovieTile
+          key={movie.id}
           id={movie.id}
-          coverUrl={movie["cover-url"]}
+          coverUrl={movie[ "cover-url"]}
           director={movie.director}
           runtime={movie["runtime-in-minutes"]}
           synopsis={movie.synopsis}
           title={movie.title}
-          url={movie.url}
+          infoLink={movie.url}
           year={movie.year}
-          userReview={this.state.reviews[movie.id]}
+          rating={movie.rating.toUpperCase()}
+          userReview={userReview.review}
           />
       )
     })
 
     return (
       <div>
-        <MovieTile />
+        {movieList}
       </div>
     );
   }
--- src/Components/MovieTile.js
+++ src/Components/MovieTile.js
@@ -32,21 +32,21 @@ class MovieTile extends React.Component {
       <div>
         <section className="movie-tile-container">
           <div className="movie-tile-image-container">
-            <img className="movie-tile-image" src="../../public/images/moon-2009.jpg"></img>
+            <img className="movie-tile-image" src={this.props.coverUrl}></img>
           </div>
           <div className="movie-tile-info-container">
             <div className="movie-tile-title-container">
               <div className="movie-tile-title-section-text">
-                <b className="movie-tile-list-number">1. </b><a className="movie-tile-title" href="https://www.rottentomatoes.com/m/10009075_moon">Moon</a><b className="movie-tile-year"> (2009)</b>
+                <b className="movie-tile-list-number">{this.props.id}. </b><a className="movie-tile-title" href={`${this.props.infoLink}`}>{this.props.title}</a><b className="movie-tile-year"> ({this.props.year})</b>
               </div>
             </div>
-            <div className="movie-tile-description-section">An astronaut miner extracting the precious moon gas that promises to reverse the Earths energy crisis nears the end of his three-year contract, and makes an ominous discovery in this psychological sci-fi film starring Sam Rockwell and Kevin Spacey. For three long years, Sam Bell has dutifully harvested Helium 3 for Lunar, a company that claims it holds the key to solving humankind's energy crisis. As Sam's contract comes to an end, the lonely astronaut looks forward...</div>
+            <div className="movie-tile-description-section">{this.props.synopsis}</div>
             <div className="movie-tile-people-section">
-              <div className="movie-tile-people-director">Director: Duncan Jones</div>
-              <div className="movie-tile-mpaa-rating">Rating: R</div>
+              <div className="movie-tile-people-director">Director: {this.props.director}</div>
+              <div className="movie-tile-mpaa-rating">Rating: {this.props.rating}</div>
               <button onClick={this.toggleDescription} className="read-review-button">Read Review</button>
             </div>
-            <div className={this.state.showDescription ? "show": "hide"}>"Duncan Jones' second film, equally smart science fiction as Moon. A really fun action-y mental trip through a seemingly normal sequence of events that gets progressively weirder as you dive deeper into the rabbit hole. Great film, great acting, great directing."</div>
+            <div className={this.state.showDescription ? "show": "hide"}>{this.props.userReview}</div>
           </div>
         </section>
       </div>
